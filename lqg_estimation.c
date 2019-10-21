d5f95f866dee727b25f98ca02fae8222e7fa5cc1
Bitiquinho <consoni_2519@hotmail.com>
2019-10-21 16:34:11 -0300

    Update submodules

--- lqg_estimation.c
+++ lqg_estimation.c
@@ -24,6 +24,7 @@
 
 #include "linearizer/system_linearizer.h"
 #include "kalman/kalman_filters.h"
+#include "lqr/ilq_regulator.h"
 
 #include <math.h>
 
@@ -33,7 +34,17 @@ const char* DOF_NAMES[ DOFS_NUMBER ] = { "angle" };
 
 LinearSystem linearSystem = NULL;
 KFilter observer = NULL;
+ILQRegulator regulator = NULL;
 
+double inputsList[ DOFS_NUMBER ];
+double measuresList[ DOFS_NUMBER ];
+double statesList[ DOFS_NUMBER * 3 ];
+double impedancesList[ DOFS_NUMBER * 3 ];
+double feedbacksList[ DOFS_NUMBER ];
+
+double samplingTime = 0.0;
+
+enum ControlState controlState = CONTROL_PASSIVE;
 
 
 DECLARE_MODULE_INTERFACE( ROBOT_CONTROL_INTERFACE );
@@ -41,10 +52,21 @@ DECLARE_MODULE_INTERFACE( ROBOT_CONTROL_INTERFACE );
 
 bool InitController( const char* configurationString )
 {
+  linearSystem = SystemLinearizer_CreateSystem( 3, 1, 500 );
+  observer = Kalman_CreateFilter( 3, 1, 1 );
+  regulator = ILQR_Create( 3, 1, 0.00001 );
+  
   return true;
 }
 
-void EndController() { return; }
+void EndController() 
+{ 
+  SystemLinearizer_DeleteSystem( linearSystem );
+  Kalman_DiscardFilter( observer );
+  ILQR_Delete( regulator );
+  
+  return; 
+}
 
 size_t GetJointsNumber() { return DOFS_NUMBER; }
 
@@ -62,28 +84,47 @@ size_t GetExtraOutputsNumber( void ) { return 0; }
          
 void GetExtraOutputsList( double* outputsList ) { return; }
 
-void SetControlState( enum ControlState controlState )
+void SetControlState( enum ControlState newControlState )
 {
-  fprintf( stderr, "Setting robot control phase: %x\n", controlState );
+  fprintf( stderr, "Setting robot control phase: %x\n", newControlState );
+  
+  controlState = newControlState;
 }
 
-void RunControlStep( DoFVariables** jointMeasuresTable, DoFVariables** axisMeasuresTable, DoFVariables** jointSetpointsTable, DoFVariables** axisSetpointsTable, double elapsedTime )
+void RunControlStep( DoFVariables** jointMeasuresList, DoFVariables** axisMeasuresList, DoFVariables** jointSetpointsList, DoFVariables** axisSetpointsList, double elapsedTime )
 {
-  axisMeasuresTable[ 0 ]->position = jointMeasuresTable[ 0 ]->position;
-  axisMeasuresTable[ 0 ]->velocity = jointMeasuresTable[ 0 ]->velocity;
-  axisMeasuresTable[ 0 ]->acceleration = jointMeasuresTable[ 0 ]->acceleration;
-  axisMeasuresTable[ 0 ]->force = jointMeasuresTable[ 0 ]->force;
-  axisMeasuresTable[ 0 ]->stiffness = jointMeasuresTable[ 0 ]->stiffness;
-  axisMeasuresTable[ 0 ]->damping = jointMeasuresTable[ 0 ]->damping;
+  samplingTime += elapsedTime;
   
-  jointSetpointsTable[ 0 ]->position = axisSetpointsTable[ 0 ]->position;
-  jointSetpointsTable[ 0 ]->velocity = axisSetpointsTable[ 0 ]->velocity;
-  jointSetpointsTable[ 0 ]->acceleration = axisSetpointsTable[ 0 ]->acceleration;
-  jointSetpointsTable[ 0 ]->force = axisSetpointsTable[ 0 ]->force;
-  jointSetpointsTable[ 0 ]->stiffness = axisSetpointsTable[ 0 ]->stiffness;
-  jointSetpointsTable[ 0 ]->damping = axisSetpointsTable[ 0 ]->damping;
+  axisMeasuresList[ 0 ]->position = jointMeasuresList[ 0 ]->position;
+  axisMeasuresList[ 0 ]->velocity = jointMeasuresList[ 0 ]->velocity;
+  axisMeasuresList[ 0 ]->acceleration = jointMeasuresList[ 0 ]->acceleration;
+  axisMeasuresList[ 0 ]->force = jointMeasuresList[ 0 ]->force;
   
-  double stiffness = jointSetpointsTable[ 0 ]->position; 
-  double positionError = jointSetpointsTable[ 0 ]->position - jointMeasuresTable[ 0 ]->position;
-  jointSetpointsTable[ 0 ]->force = stiffness * positionError;
+  inputsList[ 0 ] = axisMeasuresList[ 0 ]->force;
+  Kalman_Predict( observer, inputsList, statesList );
+  measuresList[ 0 ] = axisMeasuresList[ 0 ]->position - axisSetpointsList[ 0 ]->position;
+  Kalman_Update( observer, measuresList, statesList );
+  ILQR_CalculateFeedback( regulator, statesList, feedbacksList );
+  statesList[ 1 ] = axisMeasuresList[ 0 ]->velocity;
+  statesList[ 2 ] = axisMeasuresList[ 0 ]->acceleration;
+  SystemLinearizer_AddSample( linearSystem, statesList, inputsList );
+  if( SystemLinearizer_Identify( linearSystem, impedancesList ) )
+  {
+    Kalman_SetTransitionFactor( observer, 2, 1, -impedancesList[ 1 ] / impedancesList[ 2 ] );
+    ILQR_SetTransitionFactor( regulator, 2, 1, -impedancesList[ 1 ] / impedancesList[ 2 ] );
+    Kalman_SetInputFactor( observer, 2, 0, 1.0 / impedancesList[ 2 ] );
+    ILQR_SetInputFactor( regulator, 2, 0, 1.0 / impedancesList[ 2 ] );
+  }
+  axisMeasuresList[ 0 ]->stiffness = impedancesList[ 0 ];
+  axisMeasuresList[ 0 ]->damping = impedancesList[ 1 ];
+  axisMeasuresList[ 0 ]->inertia = impedancesList[ 2 ];
+  if( controlState == CONTROL_OPERATION ) axisSetpointsList[ 0 ]->force = feedbacksList[ 0 ];
+  else if( controlState == CONTROL_PREPROCESSING ) axisSetpointsList[ 0 ]->force = 10.0 * ( sin( elapsedTime ) - axisMeasuresList[ 0 ]->position );
+  
+  jointSetpointsList[ 0 ]->position = axisSetpointsList[ 0 ]->position;
+  jointSetpointsList[ 0 ]->velocity = axisSetpointsList[ 0 ]->velocity;
+  jointSetpointsList[ 0 ]->acceleration = axisSetpointsList[ 0 ]->acceleration;
+  jointSetpointsList[ 0 ]->force = axisSetpointsList[ 0 ]->force;
+  jointSetpointsList[ 0 ]->stiffness = axisSetpointsList[ 0 ]->stiffness;
+  jointSetpointsList[ 0 ]->damping = axisSetpointsList[ 0 ]->damping;
 }
